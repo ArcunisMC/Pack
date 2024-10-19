23a1dde3feed8a7167a03d464808ff69c9c44e9c
kay <titulaerkay@gmail.com>
2024-10-19 13:07:28 +0200

    Delete build.ps1

--- build.ps1
+++
@@ -1,5 +0,0 @@
-Remove-Item C:/Users/titul/AppData/Roaming/PrismLauncher/instances/Main/.minecraft/resourcepacks/pack -Recurse -Force
-Remove-Item C:/Users/titul/AppData/Roaming/PrismLauncher/instances/Main/.minecraft/saves/Datapack/datapacks/pack -Recurse -Force
-
-Copy-Item . C:/Users/titul/AppData/Roaming/PrismLauncher/instances/Main/.minecraft/resourcepacks/ -Recurse
-Copy-Item . C:/Users/titul/AppData/Roaming/PrismLauncher/instances/Main/.minecraft/saves/Datapack/datapacks/ -Recurse
